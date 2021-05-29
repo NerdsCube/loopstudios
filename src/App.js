328221e9be1d5d60b37e831c750a2443c4e762aa
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-29 13:05:04 +0100

    add Interactive component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header";
+import Interactive from "./components/Interactive";
 
 function App() {
   return (
     <>
       <Header />
+      <Interactive />
     </>
   );
 }
---
+++ src/components/Interactive.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import interactive from '../images/desktop/image-interactive.jpg'
+
+function Interactive() {
+    return (
+        <div>
+            <div><img src={interactive}/></div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Interactive
