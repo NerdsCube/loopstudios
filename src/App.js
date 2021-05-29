675d96a0d25251d9e40d4aecb7f94b86195f0241
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-05-29 13:21:33 +0100

    add creation component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Interactive from "./components/Interactive";
+import Creation from "./components/Creation";
 
 function App() {
   return (
     <>
       <Header />
       <Interactive />
+      <Creation />
     </>
   );
 }
---
+++ src/components/Creation.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import image1 from '../images/desktop/image-curiosity.jpg'
+import image2 from '../images/desktop/image-deep-earth.jpg'
+import image3 from '../images/desktop/image-fisheye.jpg'
+import image4 from '../images/desktop/image-from-above.jpg'
+import image5 from '../images/desktop/image-grid.jpg'
+import image6 from '../images/desktop/image-night-arcade.jpg'
+import image7 from '../images/desktop/image-pocket-borealis.jpg'
+import image8 from '../images/desktop/image-soccer-team.jpg'
+
+function Creation() {
+    return (
+        <div className="creation">
+            <div>
+                <h1>CREATION</h1>
+                <button>See All</button>
+            </div>
+            <div className="creations">
+                <img />
+            </div>            
+        </div>
+    )
+}
+
+export default Creation
