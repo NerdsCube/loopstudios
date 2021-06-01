d9b4615f526f323ccee0c0d9ecfacf96617b52f5
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-01 11:43:37 +0100

    update component

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -10,14 +10,14 @@ function Creation() {
                 <button className="btn">See All</button>
             </div>
             <div className="creations">
-                <a href="#" className="grid"><div className="one"><label>DEEP EARTH</label></div></a>
-                <a href="#" className="grid"><div className="two"><label>NIGHT ARCADE</label></div></a>
-                <a href="#" className="grid"><div className="three"><label>SOCCER TEAM VR</label></div></a>
-                <a href="#" className="grid"><div className="four"><label>THE GRID</label></div> </a>
-                <a href="#" className="grid"><div className="five"><label>FROM UP ABOVE</label></div></a>
-                <a href="#" className="grid"><div className="six"><label>POCKET BOREALIS</label></div></a>
-                <a href="#" className="grid"><div className="seven"><label>THE CURIOSITY</label></div></a>
-                <a href="#" className="grid"><div className="eight"><label>MAKE IT FISHEYE</label></div></a>
+                <div className="grid one"><label>DEEP EARTH</label></div>
+                <div className="grid two"><label>NIGHT ARCADE</label></div>
+                <div className="grid three"><label>SOCCER TEAM VR</label></div>
+                <div className="grid four"><label>THE GRID</label></div> 
+                <div className="grid five"><label>FROM UP ABOVE</label></div>
+                <div className="grid six"><label>POCKET BOREALIS</label></div>
+                <div className="grid seven"><label>THE CURIOSITY</label></div>
+                <div className="grid eight"><label>MAKE IT FISHEYE</label></div>
                 <button className="btn-down">See All</button>
             </div>            
         </div>
