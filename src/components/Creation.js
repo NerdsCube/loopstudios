c1bda225007aaea27368c68f953eb132cf45026e
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-01 01:39:04 +0100

    update creation component style

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -1,12 +1,5 @@
 import React from 'react'
-import image1 from '../images/desktop/image-curiosity.jpg'
-import image2 from '../images/desktop/image-deep-earth.jpg'
-import image3 from '../images/desktop/image-fisheye.jpg'
-import image4 from '../images/desktop/image-from-above.jpg'
-import image5 from '../images/desktop/image-grid.jpg'
-import image6 from '../images/desktop/image-night-arcade.jpg'
-import image7 from '../images/desktop/image-pocket-borealis.jpg'
-import image8 from '../images/desktop/image-soccer-team.jpg'
+
 
 function Creation() {
     return (
@@ -17,13 +10,13 @@ function Creation() {
             </div>
             <div className="creations">
                 <div className="grid one"><label>DEEP EARTH</label></div>
-                <div className="grid two"><img className="image2" src={image6} /><label>NIGHT ARCADE</label></div>
-                <div className="grid"><img className="image3" src={image8} /><label>SOCCER TEAM VR</label></div>
-                <div className="grid"><img className="image4" src={image5} /><label>THE GRID</label></div>
-                <div className="grid"><img className="image5" src={image4} /><label>FROM UP ABOVE</label></div>
-                <div className="grid"><img className="image6" src={image7} /><label>POCKET BOREALIS</label></div>
-                <div className="grid"><img className="image7" src={image1} /><label>THE CURIOSITY</label></div>
-                <div className="grid"><img className="image8" src={image3} /><label>MAKE IT FISHEYE</label></div>
+                <div className="grid two"><label>NIGHT ARCADE</label></div>
+                <div className="grid three"><label>SOCCER TEAM VR</label></div>
+                <div className="grid four"><label>THE GRID</label></div>
+                <div className="grid five"><label>FROM UP ABOVE</label></div>
+                <div className="grid six"><label>POCKET BOREALIS</label></div>
+                <div className="grid seven"><label>THE CURIOSITY</label></div>
+                <div className="grid eight"><label>MAKE IT FISHEYE</label></div>
             </div>            
         </div>
     )
