bc319a751dad6954f06ba6e84b693209570c4b21
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-01 11:11:12 +0100

    adjust creation and header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.svg'
 import hamburger from '../images/icon-hamburger.svg'
 import '../style.css'
 
 function Header() {
+    const [navbar, setNavbar] = useState(false)
+
+    const toggle = () => {setNavbar(!navbar)}
     return (
         <div className="background">
-            <nav className="desktop-nav">
+            <nav className={ navbar ? "mobile-nav" : 'desktop-nav' }>
                 <div className="logo"><img src={logo} /></div>
-                <span className="toggle"><img src={hamburger} /></span>
+                <span className="toggle"><img src={hamburger} onClick={toggle} /></span>
                 <ul className="list">
                     <li className="list-item"><a href="#" className="list-anchor">About</a></li>
                     <li className="list-item"><a href="#" className="list-anchor">Careers</a></li>
