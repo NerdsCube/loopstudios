7a06578a5a5caa1bfcd7a734a66f3a1b460d991a
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-01 03:35:41 +0100

    lay images using grid

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -6,7 +6,7 @@ function Interactive() {
         <div className="interactive">
             <div className="inter-img"><img  src={interactive}/></div>
             <div className="sm">
-                <h2>The leader in interactive VR,</h2>
+                <h1>THE LEADER IN INTERACTIVE VR,</h1>
                 <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
             </div>
         </div>
