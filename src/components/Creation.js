7f887a357202e896f150d80220cdb61889d5b340
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-03 13:55:02 +0100

    apply fonts

--- src/components/Creation.js
+++ src/components/Creation.js
@@ -6,18 +6,18 @@ function Creation() {
     return (
         <div className="creation">
             <div className="create">
-                <h1>CREATION</h1>
+                <h1>OUR CREATION</h1>
                 <button className="btn">See All</button>
             </div>
             <div className="creations">
-                <div className="grid one"><label>DEEP EARTH</label></div>
-                <div className="grid two"><label>NIGHT ARCADE</label></div>
-                <div className="grid three"><label>SOCCER TEAM VR</label></div>
-                <div className="grid four"><label>THE GRID</label></div> 
-                <div className="grid five"><label>FROM UP ABOVE</label></div>
-                <div className="grid six"><label>POCKET BOREALIS</label></div>
-                <div className="grid seven"><label>THE CURIOSITY</label></div>
-                <div className="grid eight"><label>MAKE IT FISHEYE</label></div>
+                <div className="grid one"><label>DEEP<br /> EARTH</label></div>
+                <div className="grid two"><label>NIGHT <br /> ARCADE</label></div>
+                <div className="grid three"><label>SOCCER <br /> TEAM VR</label></div>
+                <div className="grid four"><label>THE <br /> GRID</label></div> 
+                <div className="grid five"><label>FROM UP <br /> ABOVE VR</label></div>
+                <div className="grid six"><label>POCKET <br /> BOREALIS</label></div>
+                <div className="grid seven"><label>THE <br /> CURIOSITY</label></div>
+                <div className="grid eight"><label>MAKE IT <br /> FISHEYE</label></div>
                 <button className="btn-down">See All</button>
             </div>            
         </div>
