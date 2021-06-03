35005532baf14e3314228a06ff6983dade6abafd
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-06-03 14:21:18 +0100

    refactoring

--- src/components/Interactive.js
+++ src/components/Interactive.js
@@ -3,11 +3,13 @@ import interactive from '../images/desktop/image-interactive.jpg'
 
 function Interactive() {
     return (
-        <div className="interactive">
-            <div className="inter-img"><img  src={interactive}/></div>
-            <div className="sm">
-                <h1>THE LEADER IN INTERACTIVE VR,</h1>
-                <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+        <div>
+            <div className="interactive">
+                <div className="inter-img"><img  src={interactive}/></div>
+                <div className="sm">
+                    <h1>THE LEADER IN INTERACTIVE VR,</h1>
+                    <p>Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+                </div>
             </div>
         </div>
     )
